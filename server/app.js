498978088bedadaecfedc33b2101347233517743
shahparth36 <parthshah1936@gmail.com>
2023-05-09 11:19:58 +0530

    initial setup complete

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/api/test", (req, res) => res.send("Working fine!"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
